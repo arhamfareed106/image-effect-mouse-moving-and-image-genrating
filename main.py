94d4e1749e3251546e10236ee4ee882e6ec681b2
arhamfareed106 <arhamfareed575@gmail.com>
2024-09-23 03:28:16 +0500

    python

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-from math import cos
+# from math import cos
 
 
-weight = int(input("Weight: ")) 
+# weight = int(input("Weight: ")) 
 
-if weight<=1:
-    cost=5
-elif weight<=5:
-    cost=10
-elif weight<=10:
-    cost=15
-elif weight<=20:
-    cost=20
-else:
-    cost=0
+# if weight<=1:
+#     cost=5
+# elif weight<=5:
+#     cost=10
+# elif weight<=10:
+#     cost=15
+# elif weight<=20:
+#     cost=20
+# else:
+#     cost=0
 
-print("cost of shipping: ",cost)
+# print("cost of shipping: ",cost)
+
+
+# grade= int(input("Enter your grade: "))
+
+# if grade>= 80:
+#     print("A")
+# elif grade>= 70:
+#     print("B")
+# elif grade>= 60:
+#     print("C")
+
+# elif grade>= 50:
+#     print("D")
+# else:
+#     print("F")   
+
